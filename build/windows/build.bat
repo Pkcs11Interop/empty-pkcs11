3d11e318a3f11476046b6a44d750d9533fc62d80
Jaroslav Imrich <jimrich@jimrich.sk>
2019-01-27 21:33:13 +0100

    Setup Windows build

--- build/windows/build.bat
+++ build/windows/build.bat
@@ -4,14 +4,14 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\vsvars32.bat"
 
 @rem Build for x86 platform
-msbuild pkcs11-mock.sln /p:Configuration=Release /p:Platform=Win32 /target:Clean || goto :error
-msbuild pkcs11-mock.sln /p:Configuration=Release /p:Platform=Win32 /target:Build || goto :error
-copy .\Win32\Release\pkcs11-mock-x86.dll . || goto :error
+msbuild empty-pkcs11.sln /p:Configuration=Release /p:Platform=Win32 /target:Clean || goto :error
+msbuild empty-pkcs11.sln /p:Configuration=Release /p:Platform=Win32 /target:Build || goto :error
+copy .\Win32\Release\empty-pkcs11-x86.dll . || goto :error
 
 @rem Build for x64 platform
-msbuild pkcs11-mock.sln /p:Configuration=Release /p:Platform=x64 /target:Clean || goto :error
-msbuild pkcs11-mock.sln /p:Configuration=Release /p:Platform=x64 /target:Build || goto :error
-copy .\x64\Release\pkcs11-mock-x64.dll . || goto :error
+msbuild empty-pkcs11.sln /p:Configuration=Release /p:Platform=x64 /target:Clean || goto :error
+msbuild empty-pkcs11.sln /p:Configuration=Release /p:Platform=x64 /target:Build || goto :error
+copy .\x64\Release\empty-pkcs11-x64.dll . || goto :error
 
 @echo *** BUILD SUCCESSFUL ***
 @endlocal
