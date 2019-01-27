82db757b94a36d6f6847affdd6efed4b61c4ebd4
Jaroslav Imrich <jimrich@jimrich.sk>
2019-01-27 21:44:34 +0100

    Windows solution updated for VS 2017

--- build/windows/build.bat
+++ build/windows/build.bat
@@ -1,20 +1,20 @@
-@setlocal
-
-@rem Initialize build environment of Visual Studio 2015
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\vsvars32.bat"
-
 @rem Build for x86 platform
+@setlocal
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
 msbuild empty-pkcs11.sln /p:Configuration=Release /p:Platform=Win32 /target:Clean || goto :error
 msbuild empty-pkcs11.sln /p:Configuration=Release /p:Platform=Win32 /target:Build || goto :error
 copy .\Win32\Release\empty-pkcs11-x86.dll . || goto :error
+@endlocal
 
 @rem Build for x64 platform
+@setlocal
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 msbuild empty-pkcs11.sln /p:Configuration=Release /p:Platform=x64 /target:Clean || goto :error
 msbuild empty-pkcs11.sln /p:Configuration=Release /p:Platform=x64 /target:Build || goto :error
 copy .\x64\Release\empty-pkcs11-x64.dll . || goto :error
+@endlocal
 
 @echo *** BUILD SUCCESSFUL ***
-@endlocal
 @exit /b %errorlevel%
 
 :error
