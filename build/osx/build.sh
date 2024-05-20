1ec4104df99959acee5f1bd8db7cd07f7f971497
Jaroslav Imrich <jimrich@jimrich.sk>
2024-05-20 22:43:00 +0200

    macOS no longer builds x86 binaries

--- build/osx/build.sh
+++ build/osx/build.sh
@@ -4,11 +4,6 @@ set -e
 
 make distclean
 
-cat Makefile | sed 's/^ARCH_FLAGS=.*/ARCH_FLAGS= -m32/' | sed 's/^LIBNAME=.*/LIBNAME=empty-pkcs11-x86.dylib/' > Makefile.x86
-make -f Makefile.x86
-rm Makefile.x86
-make clean
-
 cat Makefile | sed 's/^ARCH_FLAGS=.*/ARCH_FLAGS= -m64/' | sed 's/^LIBNAME=.*/LIBNAME=empty-pkcs11-x64.dylib/' > Makefile.x64
 make -f Makefile.x64
 rm Makefile.x64
