98b546852758d0d1009101e2ce8d177be98b9c63
Jaroslav Imrich <jimrich@jimrich.sk>
2024-05-20 23:28:07 +0200

    Build separate libraries for simulator and real iOS devices

--- build/ios/build.sh
+++ build/ios/build.sh
@@ -6,9 +6,7 @@ rm -Rf build
 rm -Rf empty-pkcs11*.a
 
 xcodebuild -project empty-pkcs11.xcodeproj -target empty-pkcs11 -sdk iphonesimulator -configuration Release clean build
-cp build/Release-iphonesimulator/libempty-pkcs11.a libempty-pkcs11-i386.a
+cp build/Release-iphonesimulator/libempty-pkcs11.a libempty-pkcs11-iphonesimulator.a
 
 xcodebuild -project empty-pkcs11.xcodeproj -target empty-pkcs11 -sdk iphoneos -configuration Release clean build
-cp build/Release-iphoneos/libempty-pkcs11.a libempty-pkcs11-arm.a
-
-lipo -create -output libempty-pkcs11.a libempty-pkcs11-i386.a libempty-pkcs11-arm.a
+cp build/Release-iphoneos/libempty-pkcs11.a libempty-pkcs11-iphoneos.a
